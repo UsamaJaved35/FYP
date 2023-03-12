f9cf508a8dfa0505d03fd5c73f066f28263539c9
Usama <usamamughal4949@gmail.com>
2023-03-12 23:23:52 +0500

    Image text extraction completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,45 +1,61 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-const multer=require('multer');
-const Tesseract=require('tesseract.js');
-
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'./uploads/');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const multer = require('multer');
+const Tesseract = require('tesseract.js');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server,  { cors: { origin: '*' } });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
     },
-    filename:(req,file,cb)=>{
-        cb(null,`${file.originalname}`);
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`);
     }
 });
 
-const upload=multer({storage:storage});
+const upload = multer({ storage: storage });
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-const port=process.env.PORT || 4001;
-app.listen(port,()=>{
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }))
+const port = process.env.PORT || 4003;
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-app.post('/',(req,res)=>{
-res.send('hello')
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+app.post('/', (req, res) => {
+    res.send('hello')
 })
-app.post('/upload',upload.single('file'),(req,res)=>{
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    const emitProgress = (progress) => {
+        io.emit('progress', progress);
+    };
     try {
+        // console.log(req.file);
         Tesseract.recognize(
-            'uploads/'+req.file.filename,
+            'uploads/' + req.file.filename,
             'eng',
-            {logger:m=>console.log(m)}
-        ).then(({data:{text}})=>{
-            console.log(text);
-            //res.send(text);
-            return res.json({
-                message:text
+            { logger: (m) => {
+                if (m.progress) {
+                  emitProgress(m.progress);
+            }}}
+        ).then(({ data: { text } }) => {
+                console.log(text);
+                 io.emit('text',text);
+                return res.json({
+                    message: text
+                })
             })
-        })
     } catch (error) {
         res.send(error);
     }
-    //console.log(req.file);
-    //res.send('File uploaded successfully');
 });
--- Frontend/src/Components/FileUpload.js
+++ Frontend/src/Components/FileUpload.js
@@ -1,60 +1,97 @@
 import React, { useState } from 'react';
+import {io} from 'socket.io-client';
+import { useEffect } from 'react';
 import { MDBProgress, MDBProgressBar } from 'mdb-react-ui-kit';
-import Tesseract from 'tesseract.js';
+const socket = io('http://localhost:4003');
 const FileUpload = () => {
     const [Image,setImage]=useState('');
     const [loading,IsLoading]=useState(false)
     const [text,setText]=useState('')   
+    const [success,setSuccess]=useState(true)
     const [progress,setProgress]=useState(0)
-    const handleSubmit=()=>{
-        IsLoading(true);
-        console.log(loading);
-        Tesseract.recognize(
-            Image,
-            'eng',
-            { logger: (m) =>{
-                console.log(m);
-                if(m.status === 'recognizing text') {
-                    setProgress(parseInt(m.progress * 100));
-                }
-            }
-            }
-          ).then((result) => {
-            console.log(result.data);
-            setText(result.data.text);
-            IsLoading(false);
-          });
-          console.log(text)
-        //IsLoading(false);
+    useEffect(() => {
+      socket.on('connect', () => {
+        console.log('Connected to server');
+      });
+      socket.on('progress', (progress) => {
+       // IsLoading(true)
+        setProgress(parseInt(progress*100));
+      });
+       socket.on('text', (data) => {
+         setText(data.text);
+         IsLoading(false) });
+      // socket.on('disconnect', () => {
+      //   console.log('Disconnected from server');
+      // });
+      // return () => {
+      //   socket.disconnect();
+      // };
+    }, []);
+
+
+     const  handleSubmit= async()=>{
+      const formData = new FormData();  
+      formData.append('file', Image);
+      setProgress(0)
+      IsLoading(true)
+      const response=await fetch('http://localhost:4003/api/upload',
+      {
+        method:'POST',
+        body:formData
+      });
+      //then(response=>{
+        let data= await response.json()
+        console.log(data)
+        console.log(data.message)
+        if(data.message){
+          console.log(true)
+        setSuccess(true)
+        setText(data.message)
+        IsLoading(false)
+        }
+        else{
+          setSuccess(false)
+        }
+      //}).catch(error=>{
+        //console.log('catch')
+      //});
     }
     return (
     <div>
     <div className="container" style={{ height: '100vh' }}>
-      <div className="row h-50">
-      <h1 className='text-center'>Image to text</h1>
+      <div className="row h-50 pt-6">
+      <h1 className='text-center pt-6'>E-Stamp Verification System</h1>
         <div className="col-md-5 mx-auto d-flex flex-column justify-content-center">
+          {!success &&
+        <div className="alert alert-danger" role="alert">
+          There's some issue with the server. Please try again later.
+        </div>
+        }
         {!loading && !text &&
             <>
+             <h4 className='text-center'>Just Drop your E-Stamp Image here!</h4>
                 <input
                 type="file"
                 onChange={(e) =>
-                  setImage(URL.createObjectURL(e.target.files[0]))
+                  setImage(e.target.files[0])
                 }
-                className="form-control mb-2"
+                className="form-control mb-2 mt-4"
               />
+              {Image &&
              <input
                 type="button"
                 onClick={handleSubmit}
                 className="btn btn-primary mt-5"
-                value="Convert"
+                value="CHECK"
               />
+              }
               </>
             }
             {loading && !text &&
             <>
             <MDBProgress height='20'>
                 <MDBProgressBar width={progress} value={progress} valuemin={0} valuemax={100}>
-                    {progress}%
+               {progress}%
                 </MDBProgressBar>
             </MDBProgress>
               {/* <progress className="form-control" value={progress} max="100">
@@ -64,12 +101,15 @@ const FileUpload = () => {
             </>
           }
             { text && !loading &&
+            <>
+             <h4 className='text-center'>Below is the extracted text!</h4>
             <textarea
                 className="form-control w-100 mt-5"
-                rows="30"
+                rows="24"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
               ></textarea>
+              </>
     }
         </div>
     </div>
